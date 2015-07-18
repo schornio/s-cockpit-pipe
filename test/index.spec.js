5a96b0d42576d683259dce2a835056380b60fb03
Thomas Schorn <thomas@schorn.io>
2015-07-18 17:03:15 +0200

    Main test-method implemented

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,11 +1,47 @@
 'use strict';
 /* jslint node: true */
-/* global describe, it  */
+/* global describe, it, before, after  */
+
+var TEST_SOCKET_PATH = __dirname + '/test.socket';
+
+var fs = require('fs');
+var net = require('net');
+var stream = require('stream');
+var streamBuffers = require("stream-buffers");
+
+var expect = require('chai').expect;
 
 describe ('s-cockpit-pipe', function () {
+  process.env.S_COCKPIT_SOCKET_PATH = TEST_SOCKET_PATH;
 
   var sCockpitPipe = require(__dirname + '/../index.js');
+  var socketListener;
 
-  it ('should send log chunks from stdin to s-cockpit socket');
+  before (function (done) {
+    socketListener = net.createServer();
+    socketListener.listen(TEST_SOCKET_PATH, done);
+  });
+
+  after (function (done) {
+    fs.unlink(TEST_SOCKET_PATH, done);
+  });
+
+  it ('should send log chunks from stdin to s-cockpit socket', function (done) {
+    var fakeStdInBuffer = new streamBuffers.ReadableStreamBuffer();
+    var fakeStdIn = new stream.Readable().wrap(fakeStdInBuffer);
+
+    socketListener.on('readable', function () {
+      var chunk = socketListener.read();
+      var chunkString = chunk.toString();
+      var chunkObject = JSON.parse(chunkString);
+
+      expect(chunkObject).to.be.eql({ cs: 'Hallo Welt' });
+      done();
+    });
+
+    sCockpitPipe(fakeStdIn);
+
+    fakeStdInBuffer.put('Hallo Welt', 'utf8');
+  });
 
 });
